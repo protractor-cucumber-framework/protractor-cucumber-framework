1f678928d25174711f5df9fe8ddf436ef49803f0
Darrin Holst <darrinholst@gmail.com>
2015-11-24 18:35:59 -0600

    Use Cucumber.Cli to run features
    This changes the way we run cucumber to be the same way you would
    run it from the command line. This way we get the same default
    options that you normally would, i.e. color (see
    https://github.com/cucumber/cucumber-js/pull/457#issuecomment-157792257).
    It should be more stable as well and not break with internal
    cucumber changes.
    
    Also, instead of hacking into one of the formatters to get the
    results it uses cucumber's events to do it now.
    
    `cucumberOpts` now have a 1-to-1 mapping to cucumber cli arguments.
    For example the following options...
    
    ```javascript
      cucumberOpts: {
        format: ['json:./build/test-results/protractor.json', 'pretty'],
        require: ['features/step_definitions/**/*.js', 'features/support/**/*.js'],
        'no-source': true,
        strict: true
      },
    ```
    
    would be converted to the following cucumber command...
    
    ```
      cucumberjs \
      --format json:./build/test-results/protractor.json \
      --format pretty \
      --require features/step_definitions/foo.js \
      --require features/step_definitions/bar.js \
      --no-source \
      --strict \
      foo.feature \
      bar.feature
    ```
    
    This way there _shouldn't_ be any changes needed in this library to
    support new cucumber options.

--- README.md
+++ README.md
@@ -30,3 +30,24 @@ Contributing
 ------------
 
 Pull requests are welcome. Commits should have an appropriate message and be squashed.
+
+For Contributors
+----------------
+Clone the github repository:
+
+    git clone https://github.com/mattfritz/protractor-cucumber-framework
+    cd protractor-cucumber-framework
+    npm install
+
+Start up a selenium server. By default, the tests expect the selenium server to be running at `http://localhost:4444/wd/hub`. A selenium server can be started with `webdriver-manager`.
+
+    node_modules/.bin/webdriver-manager update --standalone
+    node_modules/.bin/webdriver-manager start
+
+The test suite runs against the included test application. Start that up with
+
+    npm start
+
+Then run the tests with
+
+    npm test
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var q = require('q'),
-    assign = require('object-assign'),
     path = require('path'),
-    glob = require('glob');
+    glob = require('glob'),
+    Cucumber = require('cucumber');
 
 /**
  * Execute the Runner's test cases through Cucumber.
@@ -11,146 +11,84 @@ var q = require('q'),
  * @return {q.Promise} Promise resolved with the test results
  */
 exports.run = function(runner, specs) {
-  // TODO - add the event interface for cucumber.
-  var Cucumber = require('cucumber');
-  var configDir = runner.getConfig().configDir;
-
-  // Set up exec options for Cucumber
-  var execOptions = assign({
-    format: [],
-    tags: [],
-    require: [],
-    compiler: []
-  }, runner.getConfig().cucumberOpts);
-
-  ['compiler', 'format', 'tags', 'require'].forEach(function (option) {
-    // Make sure that options values are arrays
-    if (!Array.isArray(execOptions[option])) {
-      execOptions[option] = [ execOptions[option] ];
-    }
-    if (option === 'require') {
-      execOptions[option] =
-        execOptions[option].map(function(path) {
-          // Handle glob matching
-          return glob.sync(path, {cwd: configDir});
-        }).reduce(function(opts, globPaths) {
-          // Combine paths into flattened array
-          return opts.concat(globPaths);
-        }, []).map(function(requirePath) {
-          // Resolve require absolute path
-          return path.resolve(configDir, requirePath)
-        }).filter(function(item, pos, orig) {
-          // Make sure requires are unique
-          return orig.indexOf(item) == pos;
-        });
-    }
-  });
-
-  var testResult = [];
-  var stepResults = {
-    description: null,
-    assertions: [],
-    duration: 0
-  };
-  var scenarioFailed = false;
-
-  var failedCount = 0;
-  // Add a listener into cucumber so that protractor can find out which
-  // steps passed/failed
-  var addResultListener = function(formatter) {
-    var feature = { getName: function() { return ''; } };
-    var originalHandleBeforeFeatureEvent = formatter.handleBeforeFeatureEvent;
-    formatter.handleBeforeFeatureEvent = function(event, callback) {
-      feature = event.getPayloadItem('feature');
-      if (typeof originalHandleBeforeFeatureEvent == 'function') {
-        originalHandleBeforeFeatureEvent.apply(formatter, arguments);
-      } else {
-        callback();
-      }
-    };
-    var originalHandleAfterScenarioEvent = formatter.handleAfterScenarioEvent;
-    formatter.handleAfterScenarioEvent = function(event, callback) {
-      var scenarioInfo = {
-        name: event.getPayloadItem('scenario').getName(),
-        category: feature.getName()
-      };
-      stepResults.description = scenarioInfo.name;
-      if (scenarioFailed) {
-        ++failedCount;
-        runner.emit('testFail', scenarioInfo);
-      } else {
-        runner.emit('testPass', scenarioInfo);
-      }
-
-      testResult.push(stepResults);
-      stepResults = {
-        description: null,
-        assertions: [],
-        duration: 0
-      };
-      scenarioFailed = false;
-
-      if (originalHandleAfterScenarioEvent
-          && typeof(originalHandleAfterScenarioEvent) === 'function') {
-            originalHandleAfterScenarioEvent(event, callback);
-      } else {
-        callback();
-      }
-    };
-
-    var originalHandleStepResultEvent = formatter.handleStepResultEvent;
-    formatter.handleStepResultEvent = function(event, callback) {
-      var stepResult = event.getPayloadItem('stepResult');
-      var isStepFailed = stepResult.getStatus() === Cucumber.Status.FAILED;
-      var isStepSuccessful = stepResult.getStatus() === Cucumber.Status.PASSED;
-
-      if (isStepSuccessful) {
-        stepResults.assertions.push({
-          passed: true
-        });
-        stepResults.duration += stepResult.getDuration();
-      } else if (isStepFailed) {
-        scenarioFailed = true;
-        var failureMessage = stepResult.getFailureException();
-        stepResults.assertions.push({
-          passed: false,
-          errorMsg: failureMessage.message,
-          stackTrace: failureMessage.stack
-        });
-        stepResults.duration += stepResult.getDuration();
-      }
-
-      if (originalHandleStepResultEvent
-          && typeof(originalHandleStepResultEvent) === 'function') {
-            originalHandleStepResultEvent(event, callback);
-      } else {
-        callback();
-      }
-    };
-  };
+  var results = {}
+  require('./lib/runState').initialize(runner, results);
 
   return runner.runTestPreparer().then(function() {
     return q.promise(function(resolve, reject) {
-      var cucumberConf = Cucumber.Cli.Configuration(execOptions, specs);
-      var runtime = Cucumber.Runtime(cucumberConf);
-      var formatters = cucumberConf.getFormatters();
+      var cliArguments = convertOptionsToCliArguments(runner.getConfig().cucumberOpts);
+      cliArguments.push('--require', path.resolve(__dirname, 'lib', 'resultsCapturer.js'));
+      cliArguments = cliArguments.concat(specs);
 
-      addResultListener(formatters[0]);
-      formatters.forEach(runtime.attachListener.bind(runtime));
-
-      runtime.start(function() {
+      Cucumber.Cli(cliArguments).run(function (isSuccessful) {
         try {
           if (runner.getConfig().onComplete) {
             runner.getConfig().onComplete();
           }
-          resolve({
-            failedCount: failedCount,
-            specResults: testResult
-          });
+
+          resolve(results);
         } catch (err) {
           reject(err);
         }
       });
     });
   });
+
+  function convertOptionsToCliArguments(options) {
+    var cliArguments = ['node', 'cucumberjs'];
+
+    for (var option in options) {
+      var cliArgumentValues = convertOptionValueToCliValues(option, options[option]);
+
+      if (cliArgumentValues.length) {
+        cliArgumentValues.forEach(function (value) {
+          cliArguments.push('--' + option, value);
+        });
+      } else {
+        cliArguments.push('--' + option);
+      }
+    }
+
+    return cliArguments;
+  }
+
+  function convertRequireOptionValuesToCliValues(values) {
+    var configDir = runner.getConfig().configDir;
+
+    return values.map(function(path) {
+      // Handle glob matching
+      return glob.sync(path, {cwd: configDir});
+    }).reduce(function(opts, globPaths) {
+      // Combine paths into flattened array
+      return opts.concat(globPaths);
+    }, []).map(function(requirePath) {
+      // Resolve require absolute path
+      return path.resolve(configDir, requirePath)
+    }).filter(function(item, pos, orig) {
+      // Make sure requires are unique
+      return orig.indexOf(item) == pos;
+    });
+  }
+
+  function convertGenericOptionValuesToCliValues(values) {
+    return values.reduce(function (opts, value) {
+      if (value !== true) {
+        opts.push(value);
+      }
+
+      return opts;
+    }, []);
+  }
+
+  function convertOptionValueToCliValues(option, values) {
+    if (!Array.isArray(values)) {
+      values = [values];
+    }
+
+    if (option === 'require') {
+      return convertRequireOptionValuesToCliValues(values);
+    } else {
+      return convertGenericOptionValuesToCliValues(values);
+    }
+  }
 };
