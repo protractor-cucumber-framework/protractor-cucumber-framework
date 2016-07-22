255948597247bc509adf0ca68129e828f6e29fd3
Ed Snodgrass <ed.snodgrass@csgi.com>
2016-07-21 23:42:10 -0500

    Switched app to use express instead of httpster.
    Closes #44.

---
+++ testapp/server.js
@@ -0,0 +1,11 @@
+let path = require('path');
+let express = require('express');
+
+let webServerPort = process.env.HTTP_PORT || require("../spec/environment").webServerDefaultPort;
+
+let app = express();
+app.use(express.static(__dirname));
+
+app.listen(webServerPort, function() {
+  console.log('Server started on port: ' + webServerPort);
+});
