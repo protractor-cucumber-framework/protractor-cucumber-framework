b6db44d358efe5f131a9597a7e10273d41a954d9
Craig Spence <craigspence0@gmail.com>
2017-10-25 02:01:26 +1300

    Add step progress to eventDetails (#106)
    * Adding `progress` to eventDetails
    
    * Tests for adding `progress` to eventDetails

---
+++ test/progress.spec.js
@@ -0,0 +1,74 @@
+let util = require('./test_util');
+
+describe('progress', function() {
+  it('cucumber 1 provides feature progress details to protractor', function() {
+    return util
+      .runOne(
+        'test/cucumber/conf/cucumber1Conf.js --cucumberOpts.tags @sourceLocation'
+      )
+      .expectExitCode(0)
+      .expectEvents([
+        {
+          progress: {
+            step: 1,
+            steps: 2
+          }
+        },
+        {
+          progress: {
+            step: 2,
+            steps: 2
+          }
+        }
+      ])
+      .run();
+  });
+
+  it('cucumber 2 provides feature progress details to protractor', function() {
+    return util
+      .runOne(
+        'test/cucumber/conf/cucumber2Conf.js --cucumberOpts.tags @cucumber2'
+      )
+      .cucumberVersion2()
+      .expectExitCode(0)
+      .expectEvents([
+        {
+          progress: {
+            step: 1,
+            steps: 2
+          }
+        },
+        {
+          progress: {
+            step: 2,
+            steps: 2
+          }
+        }
+      ])
+      .run();
+  });
+
+  it('cucumber 3 provides feature progress details to protractor', function() {
+    return util
+      .runOne(
+        'test/cucumber/conf/cucumber3Conf.js --cucumberOpts.tags @cucumber3'
+      )
+      .cucumberVersion3()
+      .expectExitCode(0)
+      .expectEvents([
+        {
+          progress: {
+            step: 1,
+            steps: 2
+          }
+        },
+        {
+          progress: {
+            step: 2,
+            steps: 2
+          }
+        }
+      ])
+      .run();
+  });
+});
