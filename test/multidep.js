086b5618d7104eacd3ed10488ae5ebdcc0eb4206
Jan Molak <1089173+jan-molak@users.noreply.github.com>
2021-10-05 00:49:03 +0100

    test(multidep): improved multidep installation script

--- test/multidep.js
+++ test/multidep.js
@@ -1,7 +1,7 @@
 const
-    fs           = require('fs'),
-    moduleRoot   = 'test/multidep_modules',
-    path         = require('path'),
+    fs = require('fs'),
+    moduleRoot = 'test/multidep_modules',
+    path = require('path'),
     cucumberConf = require(path.join(__dirname, '..', 'package.json')).cucumberConf;
 
 // `multidep` hasn't been updated since 2017-01-09 and doesn't support namespaced packages like `@cucumber/cucumber`,
@@ -9,18 +9,21 @@ const
 // Because of those limitations, we need to create parent directories for our test packages ourselves:
 Object.values(cucumberConf)
     .map(conf => path.join(__dirname, '..', moduleRoot, `${ conf.module }-${ conf.version }`))
-    .map(pathToModule => path.resolve(pathToModule, '..'))
-    .forEach(parentDirectory => {
-      fs.mkdirSync(parentDirectory, { recursive: true });
+    .forEach(pathToModule => {
+        const parentDirectory = path.resolve(pathToModule, '..')
+        if (!fs.existsSync(parentDirectory)) {
+            console.log('Creating parent directory', parentDirectory)
+            fs.mkdirSync(parentDirectory, { recursive: true });
+        }
     });
 
 const versionsGroupedByPackage = Object.values(cucumberConf)
     .reduce((acc, current) => {
-      acc[current.module] = (acc[current.module] || []).concat(current.version)
-      return acc;
+        acc[current.module] = (acc[current.module] || []).concat(current.version)
+        return acc;
     }, {});
 
 module.exports = {
-  path: moduleRoot,
-  versions: versionsGroupedByPackage
+    path: moduleRoot,
+    versions: versionsGroupedByPackage
 };
