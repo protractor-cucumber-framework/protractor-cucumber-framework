50f60404476e5873c1a454f5aa8bf927d78878bd
Jan Molak <1089173+jan-molak@users.noreply.github.com>
2021-10-05 01:43:02 +0100

    test(multidep): re-wrote multidep install step to make it more resilient

--- test/multidep.js
+++ test/multidep.js
@@ -2,18 +2,39 @@ const
     fs = require('fs'),
     moduleRoot = 'test/multidep_modules',
     path = require('path'),
-    cucumberConf = require(path.join(__dirname, '..', 'package.json')).cucumberConf;
+    cucumberConf = require(path.join(__dirname, '..', 'package.json')).cucumberConf,
+    { spawnSync } = require('child_process');
 
 // `multidep` hasn't been updated since 2017-01-09 and doesn't support namespaced packages like `@cucumber/cucumber`,
 // or recursive directory.
 // Because of those limitations, we need to create parent directories for our test packages ourselves:
 Object.values(cucumberConf)
-    .map(conf => path.join(__dirname, '..', moduleRoot, `${ conf.module }-${ conf.version }`))
-    .forEach(pathToModule => {
-        const parentDirectory = path.resolve(pathToModule, '..')
-        if (!fs.existsSync(parentDirectory)) {
-            console.log('Creating parent directory', parentDirectory)
-            fs.mkdirSync(parentDirectory, { recursive: true });
+    .forEach(conf => {
+        const pathToModule = path.join(__dirname, '..', moduleRoot, `${ conf.module }-${ conf.version }`)
+
+        if (!fs.existsSync(pathToModule)) {
+            fs.mkdirSync(pathToModule, { recursive: true });
+            fs.writeFileSync(path.join(pathToModule, '/package.json'), JSON.stringify({
+                name: `${ conf.module }-dummy`,
+                private: true,
+                description: 'dummy',
+                repository: 'https://example.com',
+                license: 'MIT',
+                dependencies: {
+                    [conf.module]: `${ conf.version }`
+                }
+            }, null, 4))
+
+            const result = spawnSync('npm', ['install'], {
+                cwd: pathToModule,
+                shell: true,
+                stdio: 'inherit',
+            })
+
+            if (result.error || result.status !== 0) {
+                console.error(result);
+                process.exit(1);
+            }
         }
     });
 
